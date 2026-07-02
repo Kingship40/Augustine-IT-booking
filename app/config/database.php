e09ceda5ecc960e79c6989d0d997484fc9e1441a
kingship40 <kingshipijioma40@gmail.com>
2026-07-03 00:21:32 +0100

    Fix: change connection host to Supabase IPv4 compatible pooler

--- app/config/database.php
+++ app/config/database.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'driver'   => 'pgsql', 
-    'host'     => getenv('DB_HOST') ?: 'db.vflwhhwxqsysrpcgvbwl.supabase.co',
+    'host'     => getenv('DB_HOST') ?: 'aws-0-eu-west-1.pooler.supabase.com',
     'port'     => getenv('DB_PORT') ?: '5432',
     'database' => getenv('DB_DATABASE') ?: 'postgres',
     'username' => getenv('DB_USERNAME') ?: 'postgres',
